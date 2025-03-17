0bf8fc177e66960a987c8c094d1b246c52e5d264
hanjaegyeong <hjg000223@naver.com>
2025-03-17 10:35:20 +0900

    feat: 스프링 부트 프로젝트 추가

---
+++ smartcap-backend/smartcap-backend/src/main/java/kr/kro/smartcap/smartcap_backend/SmartcapBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.smartcap.smartcap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcapBackendApplication.class, args);
+	}
+
+}
---
+++ smartcap-backend/smartcap-backend/src/test/java/kr/kro/smartcap/smartcap_backend/SmartcapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.smartcap.smartcap_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
