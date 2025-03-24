349a38c90dbeaee818b3240afb522f8a6e350729
hanjaegyeong <hjg000223@naver.com>
2025-03-24 15:41:17 +0900

    feat: 스프링 부트 프로젝트 추가

---
+++ smartcap-back/smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/SmartcapBackApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.smartcap.smartcap_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcapBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcapBackApplication.class, args);
+	}
+
+}
