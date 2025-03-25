73294cb7086479e6a797f391eecb74bd7282251a
hanjaegyeong <hjg000223@naver.com>
2025-03-25 12:36:14 +0900

    feat: SpringSecurity BCryptPasswordEncoder 등록

--- smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/config/SecurityConfig.java
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -47,6 +49,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
