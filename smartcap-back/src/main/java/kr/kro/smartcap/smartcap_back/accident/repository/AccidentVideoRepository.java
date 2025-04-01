1438bd7bbdc87680165094dcfc4b266301ab3177
hanjaegyeong <hjg000223@naver.com>
2025-04-01 16:05:04 +0900

    feat: AccidentVideoRepository 추가

---
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/accident/repository/AccidentVideoRepository.java
@@ -0,0 +1,7 @@
+package kr.kro.smartcap.smartcap_back.accident.repository;
+
+import kr.kro.smartcap.smartcap_back.accident.entity.AccidentVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccidentVideoRepository extends JpaRepository<AccidentVideo, Long> {
+}
