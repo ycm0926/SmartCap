b7eb1e697583220c14fe7c73e6d3a6023cc212f8
한윤지 <hyj9908@gamil.com>
2025-04-02 19:47:03 +0900

    feat: Alarm 비즈니스 로직 1차, 2차 alarm db 저장 및 프론트 sse 연결 로직 작성.

---
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/alarm/controller/AlarmSseController.java
@@ -0,0 +1,36 @@
+package kr.kro.smartcap.smartcap_back.alarm.controller;
+
+import kr.kro.smartcap.smartcap_back.alarm.sse.AlarmSseEmitterHandler;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/alarm")
+@RequiredArgsConstructor
+public class AlarmSseController {
+
+    private final AlarmSseEmitterHandler sseEmitterHandler;
+    private static final Logger logger = LoggerFactory.getLogger(AlarmSseController.class);
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe() {
+        String clientId = UUID.randomUUID().toString();
+        logger.info("New client subscribing to alarm events: {}", clientId);
+        return sseEmitterHandler.createEmitter(clientId);
+    }
+
+    @GetMapping(value = "/subscribe/{clientId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribeWithId(@PathVariable String clientId) {
+        logger.info("Client with ID {} subscribing to alarm events", clientId);
+        return sseEmitterHandler.createEmitter(clientId);
+    }
+}
---
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/alarm/dto/AlarmResponseDto.java
@@ -0,0 +1,24 @@
+package kr.kro.smartcap.smartcap_back.alarm.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+public class AlarmResponseDto {
+    private Long alarmId;
+    private Long constructionSitesId;
+    private Integer deviceId;
+    private Map<String, Object> gps;
+    private String alarmType;
+    private String recognizedType;
+    private String createdAt;
+    private String weather;
+    private String siteName;
+    private String constructionStatus;
+}
