79099033ec60e172fadbd979212fd6a837646122
조준희 <junh9541@gmail.com>
2025-04-03 15:35:20 +0900

    feat: 파일 위치 변경

--- smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/common/util/S3Uploader.java
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/common/util/S3Uploader.java
@@ -1,4 +1,4 @@
-package kr.kro.smartcap.smartcap_back.util;
+package kr.kro.smartcap.smartcap_back.common.util;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
