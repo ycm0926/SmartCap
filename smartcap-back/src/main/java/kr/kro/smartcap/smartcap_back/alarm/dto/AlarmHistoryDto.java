5b7108092445e77972cea71763b6212f8328af61
조준희 <junh9541@gmail.com>
2025-04-03 15:04:21 +0900

    feat: 사고, 알람  dto fastapi 전송 형식 맞게 변경

--- smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/alarm/dto/AlarmHistoryDto.java
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/alarm/dto/AlarmHistoryDto.java
@@ -7,5 +7,5 @@ import lombok.Setter;
 @Setter
 public class AlarmHistoryDto {
     private String recognizedType;  // 인식된 객체 (차량, 추락, 건설자재 등)
-    private String alarmType;       // 알람 단계 (Warning, Danger 등)
+    private int alarmType;       // 알람 단계 (1-9)
 }
