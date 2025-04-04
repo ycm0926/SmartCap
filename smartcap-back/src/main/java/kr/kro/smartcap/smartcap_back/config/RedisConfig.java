130d0fed20e8f178870d7aeb5ca5b83158eed508
hanjaegyeong <hjg000223@naver.com>
2025-04-04 18:21:57 +0900

    feat: redis 패스워드 추가

--- smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/config/RedisConfig.java
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/config/RedisConfig.java
@@ -12,14 +12,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.redis.host:127.0.0.1}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.redis.port:6379}")
     private int redisPort;
 
-    @Value("${spring.redis.database}")
+    @Value("${spring.redis.database:0}")
     private int redisDatabase;
+
+    @Value("${spring.redis.password:}")
+    private String redisPassword;
+
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
