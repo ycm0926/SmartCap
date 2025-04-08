34e3a9146d2fcc2d3024b29810618fa1837c4174
조준희 <junh9541@gmail.com>
2025-04-08 18:11:20 +0900

    feat: alarm, accident 형식에 맞지 않는 데이터 받지 않게 수정

--- smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/accident/controller/AccidentController.java
+++ smartcap-back/src/main/java/kr/kro/smartcap/smartcap_back/accident/controller/AccidentController.java
@@ -2,6 +2,7 @@ package kr.kro.smartcap.smartcap_back.accident.controller;
 
 import kr.kro.smartcap.smartcap_back.accident.dto.AccidentHistoryDto;
 import kr.kro.smartcap.smartcap_back.accident.service.AccidentProcessingService;
+import kr.kro.smartcap.smartcap_back.common.util.AlarmCategoryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,12 @@ public class AccidentController {
             @RequestBody AccidentHistoryDto dto
     ) {
         try {
+            String alarmType = AlarmCategoryMapper.map(dto.getAccidentType()).getCode();
+            if(!alarmType.equals("3")){
+                return ResponseEntity
+                        .badRequest()
+                        .body("Invalid data received");
+            }
             accidentProcessingService.processAccident(deviceId, dto);
             return ResponseEntity.ok("Accident data saved successfully");
         } catch (Exception e) {
